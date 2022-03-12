90d00c2ada147c9ad29c60a92822191a743f1b13
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-12 13:47:51 +0700

    handling useEffect & adding notifications

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
+import Notification from './components/UI/Notification';
+import { uiSliceActions } from './store/ui-slice';
 import Products from './components/Shop/Products';
-import {useEffect} from 'react';
+import {useEffect, Fragment} from 'react';
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
-
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
   const cart = useSelector(state => state.cart);
   
   useEffect(() => {
-    fetch('https://react-advanced-redux-c6410-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+  const sendCartData = async () =>  {
+    dispatch(uiSliceActions.showNotification({
+      status: 'Pending',
+      title: 'Sending...',
+      message: 'sending cart data',
+    }))
+    const response = await fetch('https://react-advanced-redux-c6410-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
       method: 'PUT',
       body: JSON.stringify(cart),
     });
-  },[cart])
+    
+    if (!response.ok) {
+      throw new Error ('Sending cart data failed')
+    }
+
+    dispatch(uiSliceActions.showNotification({
+      status: 'succes',
+      title: 'Succes...',
+      message: 'Sending cart data succesfully',
+    }))
+  };
+  
+  if (isInitial) {
+    isInitial = false;
+    return;
+  }
+  
+  sendCartData().catch(error => {
+    dispatch(uiSliceActions.showNotification({
+      status: 'error',
+      title: 'Error',
+      message: 'Sending cart data failed',
+    }))
+  })
+  },[cart, dispatch])
 
   return (
-    <Layout>
-    {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+    {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+      {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
---
+++ src/components/UI/Notification.js
@@ -0,0 +1,24 @@
+import classes from './Notification.module.css';
+
+const Notification = (props) => {
+  let specialClasses = '';
+
+  if (props.status === 'error') {
+    specialClasses = classes.error;
+  }
+
+  if (props.status === 'succes') {
+    specialClasses = classes.succes;
+  }
+
+  const cssClasses = `${classes.notification} ${specialClasses}`;
+
+  return (
+    <section className={cssClasses}>
+      <h2>{props.title}</h2>
+      <p>{props.message}</p>
+    </section>
+  )
+}
+
+export default Notification;
